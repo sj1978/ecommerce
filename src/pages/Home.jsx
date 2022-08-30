4ff032bdc1d5f51978f8bb206808fc5f9cb531ca
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-08-30 16:27:36 -0300

    styled components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import styled from 'styled-components'
 
-
-const Container = styled.div` 
-  height: 60px;
- 
-`
-const Wrapper = styled.div`
-padding: 10px 20px;
-`
 
 
 
 const Home = () => {
   return (
-    <Container>
-      <Wrapper>
-      <Navbar/>
-      </Wrapper>
-
-    </Container>
+<Navbar/>
   )
 }
 
