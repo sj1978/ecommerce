a0a02b5a8169a06b2f327dc6ed13ac1fe677d96e
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-08-30 19:46:06 -0300

    add components to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Anouncement from '../components/Anouncement'
+import Slider from '../components/Slider'
+
 
 
 
 
 const Home = () => {
   return (
+    <div>
+    <Anouncement/>
 <Navbar/>
+<Slider/>
+
+</div>
   )
 }
 
